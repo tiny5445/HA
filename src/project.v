82be57bfe586086fcd20e7fe06d854c4289f7980
tiny5445 <tinytapeoutgopi@gmail.com>
2025-01-22 12:42:35 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -1,27 +1,15 @@
-/*
- * Copyright (c) 2024 Your Name
- * SPDX-License-Identifier: Apache-2.0
- */
+module tt_um_half_adder(a, b, sum, carry);
 
-`default_nettype none
+input a;
 
-module tt_um_example (
-    input  wire [7:0] ui_in,    // Dedicated inputs
-    output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
-    input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset
-);
+input b;
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+output sum;
 
-  // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+output carry;
+
+assign carry=a&b;                           –This is same as and(carry,a,b)
+
+assign sum=a^b;                             –This is same as xor(sum,a,b)
 
 endmodule
